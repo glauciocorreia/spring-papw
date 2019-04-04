98a08d6af587ad5468b8079a66db0a9ef1677283
Glaucio <glauciocorreiaa@gmail.com>
2019-04-03 21:37:03 -0300

    Exercício API com security

--- src/main/java/br/unipe/papw/config/SegurancaConfig.java
+++ src/main/java/br/unipe/papw/config/SegurancaConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableAutoConfiguration
 @EnableWebSecurity
@@ -13,19 +15,25 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/css/**", "/index").permitAll()
-		.antMatchers("/user/**").hasRole("USER").
+		http
+			.authorizeRequests()
+				.antMatchers("/css/**", "/index").permitAll()
+				.antMatchers("/cliente/**").hasRole("cliente")
+				.antMatchers("/cliente/**").hasRole("admin")
+				.antMatchers("/admin/**").hasRole("admin")
 				.and().formLogin();
-		// .loginPage("/login").failureUrl("/login-error");
+		
+		// .loginPage("login").failureUrl("/login-error");
 	}
-
+	 
 	@Autowired
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		auth.inMemoryAuthentication()
-		.withUser("user").password("123").roles("USER")
-		.and()
-		.withUser("fuji").password("123").roles("admin")		
-		;
+			.withUser("user").password(encoder.encode("user123"))
+				.roles("cliente")
+			.and()
+			.withUser("admin1").password(encoder.encode("admin321"))
+				.roles("admin");
 	}
 }
